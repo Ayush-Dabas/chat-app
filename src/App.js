b5b184d1e1aa888002ec8c98d286700bb202efad
Ayush-Dabas <aydbs23@gmail.com>
2023-02-09 11:38:20 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
   );
 }
 
